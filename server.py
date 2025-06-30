c0f41dc830fbe74ac0cdfc7e3f567f14d3f86393
PipsOsipov <vlad_osipov2002@mail.ru>
2025-06-30 18:50:22 +0300

    Implemented recording of only the ssh log to the PostgreSQL database using the psycopg2 library.

--- server.py
+++ server.py
@@ -2,6 +2,8 @@ import socket
 import yaml
 import threading
 import re
+import psycopg2
+from datetime import datetime
 
 with open('server_conf.yml', 'r') as f:
     data = yaml.safe_load(f)
@@ -12,7 +14,19 @@ with open('server_conf.yml', 'r') as f:
         print (ADDRESS, PORT)
     else:
         print("Error: server_conf.yml 'settings' error.")
+        
+    if 'database' in data and isinstance(data['database'], dict):
+        DB_NAME = data['database'].get('db_name')
+        DB_USER = data['database'].get('user')
+        DB_PASSWORD = data['database'].get('password')
+        DB_HOST = data['database'].get('host')
+        DB_PORT = data['database'].get('port')
+        print(f"database {DB_NAME} must be on {DB_HOST}:{DB_PORT}")
+    else:
+        print("Error: server_conf.yml 'database' error.")
      
+db_connect = psycopg2.connect(database=DB_NAME, user=DB_USER, password=DB_PASSWORD, host=DB_HOST, port=DB_PORT)
+db_cursor = db_connect.cursor()
      
 def handle_client(connect, addr):
     print(f"Client connected: {addr}")
@@ -52,12 +66,27 @@ def parse_ssh_log(log_line):
 
     if password_match:
         processed_line = password_match.groupdict()
-        print(f"timestamp: {processed_line['timestamp']}| host_name: {processed_line['host']}| Access status: {processed_line['status']}| User: {processed_line['user']}")
-        return
+        processed_line['timestamp'] = datetime.fromisoformat(processed_line['timestamp'])
+        db_cursor.execute(
+            """ INSERT INTO ssh_access_log (timestamp, status, client_ip, user_name) VALUES (%s, %s, %s, %s)""",
+            (processed_line['timestamp'],
+                  processed_line['status'],
+                  processed_line['ip'],
+                  processed_line['user']))
+        db_connect.commit()
+        print(f"timestamp: {processed_line['timestamp']}| Access status: {processed_line['status']}| Client_ip: {processed_line['ip']}| User: {processed_line['user']}")
+        
     elif session_match:
-        processed_line = session_match.groupdict()
-        print(f"timestamp: {processed_line['timestamp']}| host_name: {processed_line['host']}| Session status: {processed_line['status']}| User: {processed_line['user']}")
-        return
+        processed_sline = session_match.groupdict()
+        processed_sline['timestamp'] = datetime.fromisoformat(processed_sline['timestamp'])
+        db_cursor.execute(
+            """ INSERT INTO ssh_session_log (timestamp, host_name, session_status, user_name) VALUES (%s, %s, %s, %s)""",
+            (processed_sline['timestamp'],
+             processed_sline['host'],
+             processed_sline['status'],
+             processed_sline['user']))
+        db_connect.commit()
+        print(f"timestamp: {processed_sline['timestamp']}| host_name: {processed_sline['host']}| Session status: {processed_sline['status']}| User: {processed_sline['user']}")
     else:
         print(f"Unknown str: {log_line}")
 
@@ -98,7 +127,7 @@ USB_PATTERN = re.compile("^\[usb\]\s+(?P<timestamp>\d{4}-\d{2}-\d{2}T[\d:.+-]+)\
 VPN_PATTERN = re.compile(r'^\[vpn\]\s+(?P<timestamp>\d{4}-\d{2}-\d{2}T[\d:.+-]+)\s+(?P<host>\S+)\s+(?P<service>\S+)\[\d+\]:\s+(?P<message>.+)$')
 SSH_PASSWORD_PATTERN = re.compile(r'\[ssh\] (?P<timestamp>[\d\-T:\.+]+) (?P<host>\S+) \S+\[\d+\]: (?P<status>(?:Accepted|Failed) password) for (?P<user>\S+) from (?P<ip>\d+\.\d+\.\d+\.\d+) port (?P<port>\d+)')
 SSH_SESSION_PATTERN = re.compile(r'\[ssh\] (?P<timestamp>[\d\-T:\.+]+) (?P<host>\S+) \S+\[\d+\]: pam_unix\(sshd:session\): (?P<status>session (?:opened|closed)) for user (?P<user>\w+)')
-SQUID_PATTERN = re.compile(r'\[squid\]\s+(?P<timestamp>[\d.]+)\s+(?P<processing>\d+)\s+(?P<client_ip>\d{1,3}(?:\.\d{1,3}){3})\s+(?P<status_code>\w+/\d+)\s+(?P<bytes>\d+)\s+(?P<method>\w+)\s+(?P<url_port>(?:[a-zA-Z0-9._-]+|\d{1,3}(?:\.\d{1,3}){3}):\d+)\s+(?P<squid_user>\w+)\s+(?P<type>[A-Z_]+)\/(?P<dst_host>\d{1,3}(?:\.\d{1,3}){3})\s+(?P<ident>\S+)')
+SQUID_PATTERN = re.compile(r'\[squid\]\s+(?P<timestamp>[\d.]+)\s+(?P<processing>\d+)\s+(?P<client_ip>\d{1,3}(?:\.\d{1,3}){3})\s+(?P<status_code>\w+/\d+)\s+(?P<bytes>\d+)\s+(?P<method>\w+)\s+(?P<url_port>(?:[a-zA-Z0-9._-]+|\d{1,3}(?:\.\d{1,3}){3}):\d+)\s+(?P<squid_user>\w+)\s+(?P<type>[A-Z_]+)\/(?P<dst_host>(?:\-|\d{1,3}(?:\.\d{1,3}){3}))\s+(?P<ident>\S+)')
 
 BUFFER_SIZE = 1024
 
