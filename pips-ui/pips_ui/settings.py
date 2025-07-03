8baeab61402f2abb28fd51b20a46d364791de13e
PipsOsipov <vlad_osipov2002@mail.ru>
2025-07-03 11:22:18 +0300

    added database to settings.py

--- pips-ui/pips_ui/settings.py
+++ pips-ui/pips_ui/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'pips_ui.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'db_name',
+        'USER': 'user_name',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
