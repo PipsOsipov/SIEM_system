74691d1bfb6ed73ae2060893eb9f82c778a99e70
PipsOsipov <vlad_osipov2002@mail.ru>
2025-07-03 11:45:08 +0300

    Creating database model by command python3 manage.py inspectdb > logs/models.py

--- pips-ui/logs_monitoring/models.py
+++ pips-ui/logs_monitoring/models.py
@@ -1,3 +1,60 @@
 from django.db import models
 
-# Create your models here.
+
+class SquidLog(models.Model):
+    timestamp = models.DateTimeField(blank=True, null=True)
+    client_ip = models.TextField(blank=True, null=True)
+    status_code = models.TextField(blank=True, null=True)
+    method = models.TextField(blank=True, null=True)
+    url_port = models.TextField(blank=True, null=True)
+    squid_user = models.TextField(blank=True, null=True)
+    type = models.TextField(blank=True, null=True)
+    dst_host = models.TextField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'squid_log'
+
+
+class SshAccessLog(models.Model):
+    timestamp = models.DateTimeField(blank=True, null=True)
+    status = models.TextField(blank=True, null=True)
+    client_ip = models.TextField(blank=True, null=True)
+    user_name = models.TextField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'ssh_access_log'
+
+
+class SshSessionLog(models.Model):
+    timestamp = models.DateTimeField(blank=True, null=True)
+    host_name = models.TextField(blank=True, null=True)
+    session_status = models.TextField(blank=True, null=True)
+    user_name = models.TextField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'ssh_session_log'
+
+
+class UsbLog(models.Model):
+    timestamp = models.DateTimeField(blank=True, null=True)
+    host_name = models.TextField(blank=True, null=True)
+    usb_port = models.TextField(blank=True, null=True)
+    message = models.TextField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'usb_log'
+
+
+class VpnLog(models.Model):
+    timestamp = models.DateTimeField(blank=True, null=True)
+    host_name = models.TextField(blank=True, null=True)
+    service = models.TextField(blank=True, null=True)
+    message = models.TextField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'vpn_log'
