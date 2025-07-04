2ebcca919788b559f401d2e7c7b8fd5b339e3dda
PipsOsipov <vlad_osipov2002@mail.ru>
2025-07-04 18:44:09 +0300

    Added simple view func()

--- pips-ui/logs_monitoring/views.py
+++ pips-ui/logs_monitoring/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import SquidLog, SshAccessLog, SshSessionLog, UsbLog, VpnLog
 
-# Create your views here.
+def all_logs(request):
+    squid_logs = SquidLog.objects.order_by('-timestamp')[:50]
+    ssh_access_logs = SshAccessLog.objects.order_by('-timestamp')[:50]
+    ssh_session_logs = SshSessionLog.objects.order_by('-timestamp')[:50]
+    usb_logs = UsbLog.objects.order_by('-timestamp')[:50]
+    vpn_logs = VpnLog.objects.order_by('-timestamp')[:50]
+
+    context = {
+        'squid_logs': squid_logs,
+        'ssh_access_logs': ssh_access_logs,
+        'ssh_session_logs': ssh_session_logs,
+        'usb_logs': usb_logs,
+        'vpn_logs': vpn_logs,
+    }
+    return render(request, 'logs/all_logs.html', context)
