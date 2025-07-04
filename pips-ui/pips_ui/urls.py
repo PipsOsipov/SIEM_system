fcd890834c4f4f2998c8f041f4b40ea0be6144d0
PipsOsipov <vlad_osipov2002@mail.ru>
2025-07-04 18:46:34 +0300

    fixing

--- pips-ui/pips_ui/urls.py
+++ pips-ui/pips_ui/urls.py
@@ -14,9 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path
-from . import views
+from django.urls import path, include
 
 urlpatterns = [
-    path('', views.all_logs, name='all_logs'),
+    path('logs/', include('logs.urls')),
 ]
