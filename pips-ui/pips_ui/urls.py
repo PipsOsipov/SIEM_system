77e262ae5ce5c4c9f4d432fbf8e157d9e41925f4
PipsOsipov <vlad_osipov2002@mail.ru>
2025-07-04 19:27:39 +0300

    fix

--- pips-ui/pips_ui/urls.py
+++ pips-ui/pips_ui/urls.py
@@ -17,5 +17,5 @@ Including another URLconf
 from django.urls import path, include
 
 urlpatterns = [
-    path('logs_monitoring/', include('logs.urls')),
+    path('logs_monitoring/', include('logs_monitoring.urls')),
 ]
